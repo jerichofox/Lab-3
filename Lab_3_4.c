6ffa1cb390fa056cde7a499ecd3f1886c8a8b08b
Nikita Serikov <31020070+jerichofox@users.noreply.github.com>
2025-10-07 00:46:58 +0300

    Add files via upload

---
+++ Lab_3_4.c
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_DEPRECATE
+#define _CRT_NONSTDC_NO_DEPRECATE
+#include <locale.h>;
+#include <stdio.h>;
+
+void main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int k, s;
+
+	puts("Введите кол-во товара:");
+	scanf("%d", &k);
+	puts("Введите стоимость единицы товара:");
+	scanf("%d", &s);
+	printf("Стоимость покупки: %d", k * s);
+
+	return 0;
+}
